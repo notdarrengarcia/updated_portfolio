78b9af3ee3489c07f933b9be8c88c5a050fd2985
Darren Garcia <darrengarcia159@gmail.com>
2020-12-07 23:21:31 -0600

    finished website and added functional links. Also works with all screen sizes

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const bodyElement = document.querySelector('body');
 const menuToggle = document.querySelector('.hamburger-menu');
+const heroElement = document.querySelector('.hero');
+const navList = document.querySelector('.nav-list');
+ 
 
 
 menuToggle.addEventListener('click', function (){
     bodyElement.classList.toggle('is-open');
+    heroElement.classList.add('is-open');
+});
+
+navList.addEventListener('click', function (e){
+    let clickedElement = e.target;
+
+    if(clickedElement.classList[0] === "nav-link"){
+        bodyElement.classList.remove('is-open');
+    }
+});
+
+// Ad eventlistener to the window object to close nav when touched
+
+window.addEventListener('click', function (e){
+    let clickedElement = e.target;
+
+    if (clickedElement.matches('.is-open')){
+        bodyElement.classList.remove('is-open');
+        heroElement.classList.remove('is-open');
+
+    }
 });
