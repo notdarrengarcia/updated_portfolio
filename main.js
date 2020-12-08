265cc023748ad1ad9f6e20afa91cdd0c9fa8bfa2
Darren Garcia <darrengarcia159@gmail.com>
2020-12-07 18:36:26 -0600

    updated css and added the js file

---
+++ main.js
@@ -0,0 +1,7 @@
+const bodyElement = document.querySelector('body');
+const menuToggle = document.querySelector('.hamburger-menu');
+
+
+menuToggle.addEventListener('click', function (){
+    bodyElement.classList.toggle('is-open');
+});
